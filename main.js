7077946adbb9414ff82a01b2b2f7680abea7f334
Tiaguinho2002 <titi.zidoro18@gmail.com>
2025-10-15 23:26:24 -0300

    auth atualization

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-const { Client } = require("whatsapp-web.js");
-const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+// const qrcode = require("qrcode-terminal");
 
-const client = new Client();
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
 
 client.on("ready", () => {
   console.log("Client is ready!");
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
+// client.on('qr', qr => {
+//     qrcode.generate(qr, {small: true});
+// });
 
 client.on("message_create", (message) => {
-  console.log(message);
+  console.log(message.body);
   if (message.body === "!ping") {
     // send back "pong" to the chat the message was sent in
     client.sendMessage(message.from, "pong");
